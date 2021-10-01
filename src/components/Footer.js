65fefa0b95c69381f6981e4acdee15afaa842d40
rest2437 <nocturnalrd@gmail.com>
2021-09-30 17:29:55 -0700

    changes: messing with forms and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,7 +24,8 @@ function Footer() {
     <div className="footer-container">
       <section className="footer-subscription">
         <p className="footer-subscription-heading">
-          Join the Adventure newsletter to receive our best vacation deals
+          Join the Tails on Trails Newsletter to get updates on new Dogs to
+          Walk!
         </p>
         <p className="footer-subscription-text">
           You can unsubscribe at any time.
@@ -45,27 +46,27 @@ function Footer() {
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>About Us</h2>
-            <Link to="/sign-up">How it works</Link>
-            <Link to="/">Testimonials</Link>
-            <Link to="/">Careers</Link>
-            <Link to="/">Investors</Link>
-            <Link to="/">Terms of Service</Link>
+            <Link to="/sign-up">=========</Link>
+            <Link to="/">=========</Link>
+            <Link to="/">=========</Link>
+            <Link to="/">=========</Link>
+            <Link to="/">=========</Link>
           </div>
           <div class="footer-link-items">
             <h2>Contact Us</h2>
             <Link to="/">Contact</Link>
             <Link to="/">Support</Link>
-            <Link to="/">Destinations</Link>
-            <Link to="/">Sponsorships</Link>
+            <Link to="/">Donations</Link>
+            <Link to="/">=========</Link>
           </div>
         </div>
         <div className="footer-link-wrapper">
           <div class="footer-link-items">
             <h2>Videos</h2>
             <Link to="/">Submit Video</Link>
-            <Link to="/">Ambassadors</Link>
-            <Link to="/">Agency</Link>
-            <Link to="/">Influencer</Link>
+            <Link to="/">=========</Link>
+            <Link to="/">=========</Link>
+            <Link to="/">=========</Link>
           </div>
           <div class="footer-link-items">
             <h2>Social Media</h2>
