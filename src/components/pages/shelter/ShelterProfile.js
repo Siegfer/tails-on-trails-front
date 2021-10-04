526fd6317d9fbca7296ef788b44626fca842e623
rest2437 <nocturnalrd@gmail.com>
2021-10-04 14:44:27 -0700

    created dog form to add a dog

--- src/components/pages/shelter/ShelterProfile.js
+++ src/components/pages/shelter/ShelterProfile.js
@@ -1,61 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./ShelterProfile.css";
+import InputADog from "./InputADog";
 
 const ShelterProfile = (props) => {
-	const { handleLogout, user, dog } = props
-	console.log(`🧚🏽‍♂️ ~ Profile ~ props`, props)
-	const { id, name, email, volunteer, exp } = user
-	// const { breed, gender, size, characteristic, age, description } = Dog
+  const { handleLogout, user, dog } = props;
+  console.log(`🧚🏽‍♂️ ~ Profile ~ props`, props);
+  const { id, name, email, volunteer, exp } = user;
+  // const { breed, gender, size, characteristic, age, description } = Dog
 
-	const expirationTime = new Date(exp * 1000)
-	let currentTime = Date.now()
+  const expirationTime = new Date(exp * 1000);
+  let currentTime = Date.now();
 
-	// make a condition that compares exp and current time
-	if (currentTime >= expirationTime) {
-		handleLogout()
-		alert('Session has ended. Please login to continue.')
-	}
+  // make a condition that compares exp and current time
+  if (currentTime >= expirationTime) {
+    handleLogout();
+    alert("Session has ended. Please login to continue.");
+  }
 
-	const userData = user ? (
-		<div>
-			<h1>Profile</h1>
-			<p>Name: {name}</p>
-			<p>Email: {email}</p>
-			<p>ID: {id}</p>
-		</div>
-	) : (
-		<h2>Loading...</h2>
-	)
+  const userData = user ? (
+    <div className="shelter-container">
+      <div className="shelter-profile-data">
+        <h1>Profile</h1>
+        <p>Name: {name}</p>
+        <p>Email: {email}</p>
+        <p>ID: {id}</p>
+      </div>
+      <div className="shelter-dogs-form">
+        <InputADog />
+      </div>
+    </div>
+  ) : (
+    <h2>Loading...</h2>
+  );
 
-	// const allDogs = Dog ? (
-	// 	<div>
-	// 		<h1>All Dogs</h1>
-	// 		<h1>{props.name}</h1>
-	// 		<p>Breed: {breed}</p>
-	// 		<p>Gender: {gender}</p>
-	// 		<p>Size: {size}</p>
-	// 		<p>Characteristic: {characteristic}</p>
-	// 		<p>Age: {age}</p>
-	// 		<p>Description: {description}</p>
-	// 	</div>
-	// ) : (
-	// 	<h2>Loading...</h2>
-	// )
+  // const allDogs = Dog ? (
+  // 	<div>
+  // 		<h1>All Dogs</h1>
+  // 		<h1>{props.name}</h1>
+  // 		<p>Breed: {breed}</p>
+  // 		<p>Gender: {gender}</p>
+  // 		<p>Size: {size}</p>
+  // 		<p>Characteristic: {characteristic}</p>
+  // 		<p>Age: {age}</p>
+  // 		<p>Description: {description}</p>
+  // 	</div>
+  // ) : (
+  // 	<h2>Loading...</h2>
+  // )
 
-	const errorDiv = () => {
-		return (
-			<div className='text-center pt-4'>
-				<h3>
-					Please <Link to='./neutral/OneLogin'>login</Link> to view this page
-				</h3>
-			</div>
-		)
-	}
+  const errorDiv = () => {
+    return (
+      <div className="text-center pt-4">
+        <h3>
+          Please <Link to="./neutral/OneLogin">login</Link> to view this page
+        </h3>
+      </div>
+    );
+  };
 
-	return <div className='text-center pt-4'>{user ? userData : errorDiv()}</div>
-}
+  return <div className="text-center pt-4">{user ? userData : errorDiv()}</div>;
+};
 
-export default ShelterProfile
+export default ShelterProfile;
 
 // import React from 'react'
 // import Dog from './pages/dogs/Dog'
